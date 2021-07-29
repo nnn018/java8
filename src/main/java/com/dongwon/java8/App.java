3917093c05199c342afad38793e92ea4860c5271
donkim <n011@nate.com>
2021-07-29 16:38:47 +0900

    commit

--- src/main/java/com/dongwon/java8/App.java
+++ src/main/java/com/dongwon/java8/App.java
@@ -3,6 +3,7 @@ package com.dongwon.java8;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Spliterator;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
@@ -11,11 +12,22 @@ public class App {
         name.add("white");
         name.add("xxx");
         name.add("foo");
-        Spliterator<String> spliterator = name.spliterator();
-        Spliterator<String> stringSpliterator = spliterator.trySplit();
-        while (spliterator.tryAdvance(System.out::println));
-        System.out.println("=======================");
-        while (stringSpliterator.tryAdvance(System.out::println));
+
+        Stream<String> stringStream = name.stream().map(String::toUpperCase);
+        stringStream.forEach(System.out::println);
+
+//        name.sort(String::compareToIgnoreCase);
+//        name.forEach(System.out::println);
+
+//        name.removeIf(s -> s.startsWith("k"));
+//        name.forEach(System.out::println);
+
+
+//        Spliterator<String> spliterator = name.spliterator();
+//        Spliterator<String> stringSpliterator = spliterator.trySplit();
+//        while (spliterator.tryAdvance(System.out::println));
+//        System.out.println("=======================");
+//        while (stringSpliterator.tryAdvance(System.out::println));
 
 
         //name.forEach(System.out::println);

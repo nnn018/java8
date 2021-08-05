abc1cd5f9b0be5cf94777d77aab754fd88040f70
donkim <n011@nate.com>
2021-08-05 17:37:35 +0900

    commit

---
+++ src/main/java/com/dongwon/java8/OnlineClass.java
@@ -0,0 +1,12 @@
+package com.dongwon.java8;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter @AllArgsConstructor
+public class OnlineClass {
+    private Integer id;
+    private String title;
+    private boolean closed;
+}

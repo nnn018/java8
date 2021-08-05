6ff4895652eb1247ccbd375904f38170f5e03df4
donkim <n011@nate.com>
2021-08-05 19:38:24 +0900

    commit

--- src/main/java/com/dongwon/java8/StreamApp.java
+++ src/main/java/com/dongwon/java8/StreamApp.java
@@ -3,6 +3,8 @@ package com.dongwon.java8;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamApp {
     public static void main(String[] args) {
@@ -39,11 +41,26 @@ public class StreamApp {
         keesunEvents.add(javaClasses);
 
         System.out.println("두 수업 목록에 들어있는 모든 수업 아이디 출력");
+        keesunEvents.stream()
+                        .flatMap(list -> list.stream())
+                        .forEach(oc -> System.out.println("oc = " + oc.getId()));
 
         System.out.println("10부터 1씩 증가하는 무제한 스트림 중에서 앞에서 10개 빼고 최대 10개 까지만");
+        Stream.iterate(10, i -> i + 1)
+                .skip(10)
+                .limit(10)
+                .forEach(System.out::println);
 
         System.out.println("자바 수업 중에 Test가 들어있는 수업이 있는지 확인");
+        boolean test = javaClasses.stream().anyMatch(oc -> oc.getTitle().contains("Test"));
+        System.out.println("test = " + test);
 
+        System.out.println("스프링 수업 중에 제목에 spring이 들어간 제목만 모아서 List로 만들기");
+        List<String> spring = springClasses.stream()
+                .filter(oc -> oc.getTitle().contains("spring"))
+                .map(oc -> oc.getTitle())
+                .collect(Collectors.toList());
+        System.out.println("spring = " + spring);
 
     }
 }
